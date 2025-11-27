7ec1962b38985affbdbb423a891401b5df944709
Manuel Enrique Garcia Neve <77370606+many903@users.noreply.github.com>
2025-11-26 23:17:14 -0600

    error de libreria

--- TermocicladorUI.java
+++ TermocicladorUI.java
@@ -4,6 +4,7 @@ import javax.swing.text.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
 import java.io.*;
 import java.util.ArrayList;
 import java.util.HashMap;
